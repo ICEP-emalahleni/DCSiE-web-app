baaa1fda1e88852bc8d9310347375812b17ddc6e
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2021-11-23 01:04:44 +0200

    added home.php and homestyle.css

---
+++ home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DCSIE</title>
+    <link rel='stylesheet' href='homestyle.css'>
+</head>
+<body>
+    
+<section>
+ <center><div>
+ <input class="search" type="text" placeholder="Search building or equipment here"></input>
+ <br><br><input class="searchbtn" type="submit" value="Search"></input>
+ <br><br><a class="registerlink" href="register.php" >Regiter here to book</a>
+ </div><center>
+</section>
+
+</body>
+</html>
