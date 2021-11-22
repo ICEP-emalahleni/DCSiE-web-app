d8ea1e62ad44e1df1fa434a9edcf03a174eba5c2
talifhani22 <216820266@tut4life.ac.za>
2021-11-22 14:17:01 +0200

    NewCode

--- Contact.php
+++ Contact.php
@@ -4,9 +4,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>contact</title>
 </head>
 <body>
-    <h1>Hello Marinkie</h1>
+<div class="container">
+    <form action="Contact.php">
+  
+        <h1>CONTACT US</h1>
+        <p>49 beyrl street
+           Waterfall park</p>
+           <p>icep@gmail.com</p>
+           <p>013 565 7890</p>
+      <label for="name">Name</label>
+      <input type="text" id="name" name="name" placeholder="Enter your name">
+  
+      <label for="email">Email</label>
+      <input type="text" id="email" name="email" placeholder="Enter your email">
+
+      <label for="phone">Phone</label>
+      <input type="text" id="email" name="email" placeholder="Enter your phone number">
+
+      <label for="address">Address</label>
+      <input type="text" id="address" name="address" placeholder="Enter your address">
+  
+      <label for="subject">Subject</label>
+      <input type="text" id="subject" name="subject" placeholder="Type of the subject">
+  
+      <label for="subject">Message</label>
+      <textarea id="subject" name="subject" placeholder="Type your message here..." ></textarea>
+  
+      <input type="submit" value="Submit">
+  
+    </form>
+  </div>
 </body>
 </html>
