e6f0bd33ce3480f4e39d5c54446ce98e28046745
Thandi <215706010@tut4life.ac.za>
2021-11-23 14:26:20 +0200

    tee

--- AboutUs.php
+++ AboutUs.php
@@ -1,12 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello there person</h1>
+ <html>
+  <head>
+   <title>About Us </title>
+   <meta charset = "UTF-8">
+   <meta name = "viewport" content ="width=device=width, initial-scale=1.0">
+   <link rel ="stylesheet"  type="text/css" href="styles.css">
+   </head>
+   <body>
+       <div class = "section">
+	     <div class = "container">
+	      <div class = "content -section">
+	        <div class = "title">
+             <h1>ABOUT US</h1>
+              </div>
+			   <div class = "content">
+			     <h3> DCSiE is a Digital Campus Structures Inventory initiated by ICEP emalahleni. As the ICEP team at Emalahleni we have created this project a web and mobile application that will allow users to manage the campus infrastructure.
+
+                      The current state of the infrastructure in the campus is not known and recorded which means the ability to keep track of the availability and managing the infrastructure is minimized. 
+				      The DCSIE website and application comes to the rescue as it allows the you to record and manage the campus infrastructure as it enables you to store the name, capacity, 
+					  inventory and availability of each infrastructure in each building. <h3>
+
+				    <p> The main object of this program is amongst others to assist with recording, and mananging the campus infrasture available
+				     in the campus.
+				 
+				      You are equipped with the ability to view the condition of each inventory item and the  availability
+                      of each structure. Our mission is to be a team that inspires and fulfils your curiosity.</p>			 
+				 
+			  
+			  <div class = "button">
+			    <a href ="">Read More</a>
+			  
+            </div>
+				</div>
+				
+			</div>
+			<div class="image-section">
+				<img  src="img one.jpg">
+			</div>
+		</div>
+	</div>
+
+	
 </body>
 </html>
+
